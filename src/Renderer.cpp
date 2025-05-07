1b027276fe2c05ef94dccfc3903abfc3ad8ba56b
Seger01 <sjw.sars@student.avans.nl>
2025-05-07 20:58:57 +0200

    working

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -26,14 +26,19 @@
 
 namespace SML
 {
+// : texturedQuadShader("shaders/vertex_shader.glsl", "shaders/fragment_shader.glsl"),
+//   rectShader("shaders/colored_rect/vertex_shader.glsl", "shaders/colored_rect/fragment_shader.glsl"),
+//   circleShader("shaders/circle/vertex_shader.glsl", "shaders/circle/fragment_shader.glsl"),
+//   fontShader("shaders/font_shader/vertex_shader.glsl", "shaders/font_shader/fragment_shader.glsl"), window(window)
 
-Renderer::Renderer(Window& window)
-    : texturedQuadShader("shaders/vertex_shader.glsl", "shaders/fragment_shader.glsl"),
-      rectShader("shaders/colored_rect/vertex_shader.glsl", "shaders/colored_rect/fragment_shader.glsl"),
-      circleShader("shaders/circle/vertex_shader.glsl", "shaders/circle/fragment_shader.glsl"),
-      fontShader("shaders/font_shader/vertex_shader.glsl", "shaders/font_shader/fragment_shader.glsl"), window(window)
+Renderer::Renderer(Window& window) : window(window)
 {
-    texturedQuadShader.use();
+    // Initialize shaders
+    std::string texturedQuadVertexShader = R"
+        #include "shaders / vertex_shader.glsl " 
+                                               "
+
+                                           texturedQuadShader.use();
     texturedQuadShader.setInt("texture1", 0);
 
     glDisable(GL_DEPTH_TEST);
